48217a662f724502c47f84ca90c13fe40639a196
clovisdanielcosta <clovisdanielcosta@gmail.com>
2021-09-18 13:44:17 -0300

    Updated class JogoService and include : Disposal IJogoRepository and IJogoService interfaces.

--- ApiCatalogoJogos/ApiCatalogoJogos/Services/IJogoService.cs
+++ ApiCatalogoJogos/ApiCatalogoJogos/Services/IJogoService.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 
 namespace ApiCatalogoJogos.Services
 {
-    public interface IJogoService
-    {
+    public interface IJogoService : IDisposable    {
         Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
         Task<JogoViewModel> Obter(Guid id);
         Task<JogoViewModel> Inserir(JogoInputModel jogo);
