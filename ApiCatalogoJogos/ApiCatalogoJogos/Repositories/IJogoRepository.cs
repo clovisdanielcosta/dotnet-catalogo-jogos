92dc7474d8c0b5c4f497d847bec3df83ba98d92b
clovisdanielcosta <clovisdanielcosta@gmail.com>
2021-09-18 12:51:25 -0300

    Created Entity Jogo

--- ApiCatalogoJogos/ApiCatalogoJogos/Repositories/IJogoRepository.cs
+++ ApiCatalogoJogos/ApiCatalogoJogos/Repositories/IJogoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiCatalogoJogos.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace ApiCatalogoJogos.Repositories
         Task<List<Jogo>> Obter(int pagina, int quantidade);
         Task<Jogo> Obter(Guid id);
         Task<List<Jogo>> Obter(string nome, string produtoria);
-        Task Inserir(ApiCatalogoJogos jogo);
+        Task Inserir(Jogo jogo);
         Task Atualizar(Jogo jogo);
         Task Remover(Guid id);
     }
